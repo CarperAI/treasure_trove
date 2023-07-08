5ccfd26a49f93f65ac410a649f55aa14f986f9df
Brian <cooldude242@gmail.com>
2023-07-08 22:37:53 +0000

    workign

--- gpt_labeling.py
+++ gpt_labeling.py
@@ -8,11 +8,9 @@ import time
 from treasure_trove.core import LLMLabeler, instruction
 
 load_dotenv(".env")
-api_key = os.environ["OPENAI_KEY"]
 labels = ["high quality", "medium quality", "low quality"]
 secondary_labels = ["high", "medium", "low"]
 lang = "python"
-processed_subsets = []
 max_chars = 4_096
 num_workers = 8
 labeler = LLMLabeler(
@@ -22,67 +20,68 @@ labeler = LLMLabeler(
 )
 res = labeler("def create()")
 print(res)
+dataset_chunks = []
 
-buffer_size = 10_000
-chunks_to_process = 10
+buffer_size = 500
+chunks_to_process = 20
 
 print("Loading dataset..")
-dataset = load_dataset(
-    "bigcode/the-stack-dedup",
-    data_dir=f"data/{lang}",
-    streaming=True,
-)["train"]
+dataset = load_dataset("parquet", data_files={"train": "data-00000-of-00144.parquet"})[
+    "train"
+]
 print("Loaded dataset.")
 
-subset = dataset.shuffle(seed=110, buffer_size=buffer_size)
+api_key = os.environ["OPENAI_KEY"]
 
 subset_save_interval = 100
 
-total_cost = 0
 max_failures = 5
 failures = 0
 
 ckpt_dir = "./checkpoints"
 Path(ckpt_dir).mkdir(exist_ok=True)
 
-for chunk in range(chunks_to_process):
-    print(f"Chunk {chunk} / {chunks_to_process} starting...")
 
-    processed_rows = []
-    subset.set_epoch(chunk)
+def process(x):
+    failures = 0
+    label_idx, cost_info = 0, {}
+    while failures < max_failures:
+        try:
+            label, cost_info = labeler(x["content"][:max_chars])
+            label_idx = labels.index(label)
+            print(label, label_idx)
+            time.sleep(1)
+            break
+        except Exception as e:
+            failures += 1
+            print(e)
+            time.sleep(1)
+    print(
+        f"classified {i}: {label} - tokens used: {cost_info['prompt_tokens']} | {cost_info['completion_tokens']}"
+    )
+    return {"label": label_idx, "cost": cost_info["total_cost"]}
 
-    for i, x in enumerate(subset):
-        failures = 0
-        label_idx, cost_info = 0, {}
-        while failures < max_failures:
-            try:
-                label, cost_info = labeler(x["content"][:max_chars])
-                label_idx = labels.index(label)
-                print(label, label_idx)
-                time.sleep(1)
-                break
-            except Exception as e:
-                failures += 1
-                print(e)
-                time.sleep(1)
-        if failures != max_failures:
-            total_cost += cost_info["total_cost"]
-            print(
-                f"classified {i}: {label} - tokens used: {cost_info['prompt_tokens']} | {cost_info['completion_tokens']}"
-            )
-            processed_rows.append({**x, "label": label, "language": lang})
-        else:
-            print(f"Max failures hit on idx {i}, continuing.")
-        if i % subset_save_interval == 0:
-            subset_ds = Dataset.from_list(processed_rows)
-            subset_ds.save_to_disk(os.path.join(ckpt_dir, f"chunk_{chunk}_subset_{i}"))
-            subset_ds.push_to_hub("roborovski/phi-1", private=True)
 
-    processed_subsets.append(processed_rows)
-    # Save all processed data
-    all_datasets: Dataset = concatenate_datasets(processed_subsets)
-    all_datasets.save_to_disk(os.path.join(ckpt_dir, "latest"))
+processed_chunk_datasets = []
+start_idx = 1
+
+for i in range(start_idx, start_idx + buffer_size, 1):
+    print(f"Chunk {i} / {chunks_to_process + start_idx} starting...")
+
+    subset = dataset[i : i + buffer_size]
+
+    # Label the subset
+    subset = dataset.map(process, batched=False, num_proc=8)
+
+    processed_chunk_datasets.append(subset)
+
+    all_datasets: Dataset = concatenate_datasets(processed_chunk_datasets)
     all_datasets.push_to_hub("roborovski/phi-1", private=True)
+    all_datasets.to_parquet(
+        os.path.join(
+            ckpt_dir, f"processed_{start_idx}_to_{chunks_to_process+start_idx}"
+        )
+    )
 
     # print number of each class
     print(
@@ -94,4 +93,3 @@ for chunk in range(chunks_to_process):
     print(
         f"Number of {labels[2]}: {len(all_datasets.filter(lambda x: x['label'] == 2))}"
     )
-    print(f"Cost so far: {total_cost}")
